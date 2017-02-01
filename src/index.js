752dbf592fd7868cc79712a0afcee5a44681aa83
Saiful Bashar <robal.bashar59@gmail.com>
2017-02-01 23:13:50 +0600

    Add another function postCode

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 var data = require("./dhakaDivision.json");
 var _ = require("lodash");
 
+var postCode = function (postCode) {
+    var index = _.findIndex(data,{"pc":parseInt(postCode)});
+    return {
+        district : data[index].district,
+        thana : data[index].thana,
+        subOffice:data[index].subOffice
+    };
+}
+
 var info = function(district,thana,subOffice){
     var index = _.findIndex(data,{"district":district+" ","thana":thana+" ","subOffice":subOffice+" "});
     return data[index].pc;;
 }
+
 module.exports = {
 	info : info,
+    postCode:postCode
 };
